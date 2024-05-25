adde417ed5ef0ab4beef917331ea3b501d65a439
Rajesh <rajeshvaranm2001@gmail.com>
2024-05-25 17:45:50 +0530

    Values and Variables

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,10 @@
+//Values and Variables
+let country = 'India';
+let continent = 'Asia';
+let population = 1441
+
+console.log({
+    'Country': country,
+    'Continent':continent,
+    'Population': population + ' Millions'
+})
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,3 +2,21 @@
 let js = 'amazing';
 if(js === 'amazing') alert('JavaScript is Fun!');
 console.log(40 + 8 + 23 - 10);
+
+//Values and Variables
+// A variable is a box where we can store values, later in the code we can reference the variable over and over again.
+
+//Always use camel case convention for variable names
+let firstPersonName = 'James';
+console.log(firstPersonName);
+
+//don't use below naming convention for variable names
+// let new = 'NEW';
+// let 3years = 3;
+// let James = 'James';
+// let james&Murphy = 'JM';
+
+//allowed naming conventions
+let job1 = 'Programmer';
+let job2 = 'Teacher';
+let PI = 3.14;
